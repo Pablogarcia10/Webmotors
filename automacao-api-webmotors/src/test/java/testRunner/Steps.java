6ed16b518c52fc42bd710bafc3a47ea96782c00a
Pablo Alves Garcia <pabloalvesgarcia@gmail.com>
2020-04-02 18:04:00 -0300

    Commit final

--- automacao-api-webmotors/src/test/java/testRunner/Steps.java
+++ automacao-api-webmotors/src/test/java/testRunner/Steps.java
@@ -11,7 +11,8 @@ import io.restassured.response.Response;
 
 public class Steps {
 
-	Response url;
+	String url = "http://desafioonline.webmotors.com.br/api/OnlineChallenge/Make";
+	Response resp;
 
 	@Dado("^que eu tenha acesso a API$")
 	public void que_eu_tenha_acesso_a_API() throws Throwable {
@@ -20,21 +21,21 @@ public class Steps {
 
 	@Quando("^eu enviar um GET ao endpoit\"([^\"]*)\"$")
 	public void eu_enviar_um_GET_ao_endpoit(String api) throws Throwable {
-		url = when().get(api);
-
+		when().get(api);
 	}
 
 	@Entao("^a API deve retornar o codigo (\\d+)$")
 	public void a_API_deve_retornar_o_codigo(int response) throws Throwable {
-		url.then().statusCode(200).contentType(ContentType.JSON);
+		when().get(url).then().assertThat().statusCode(response).contentType(ContentType.JSON);
 
 	}
 
 	@Quando("^eu enviar um GET para a \"([^\"]*)\"com um numero de (\\d+) a (\\d+)$")
 	public void eu_enviar_um_GET_para_a_com_um_numero_de_a(String api, int minimo, int maximo) throws Throwable {
 		for(int a = minimo; a < maximo; minimo ++){
-			url = when().get(api);
-			System.out.println(url);
+			resp = when().get(api);
+			System.out.println(resp);
+			
 		}
 	}
 }
--- automacao-web-webmotors/src/test/java/annotation/Steps.java
+++ automacao-web-webmotors/src/test/java/annotation/Steps.java
@@ -11,6 +11,7 @@ import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
 
+
 public class Steps {
 
 	@FindBy(xpath = "//h2[.='HONDA CITY']")
